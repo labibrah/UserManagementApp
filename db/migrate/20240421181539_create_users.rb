8620a5374ef20f4216a7674ecdf7cfc8b9f7f497
Labib Rahman <labib.rahman@northsouth.edu>
2024-04-26 01:04:52 +0600

    Changed test

--- db/migrate/20240421181539_create_users.rb
+++ db/migrate/20240421181539_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password_digest
