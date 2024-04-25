322ab042182726c5d984e2e1cafbc52afea21a0e
Labib Rahman <labib.rahman@northsouth.edu>
2024-04-25 20:00:48 +0600

    Adding db simple

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema[7.1].define(version: 2024_04_21_181539) do
-#   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-#     t.string "name"
-#     t.string "email"
-#     t.string "password_digest"
-#     t.datetime "last_login"
-#     t.datetime "registration_time"
-#     t.string "status"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#   end
+ActiveRecord::Schema[7.1].define(version: 2024_04_21_181539) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "last_login"
+    t.datetime "registration_time"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
-# end
+end
