db9ca94582c3c7af1fb55b9bf839125c10c3428d
Labib Rahman <labib.rahman@northsouth.edu>
2024-04-26 16:40:52 +0600

    Added unique index

--- db/migrate/20240421181539_create_users.rb
+++ db/migrate/20240421181539_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email, uniqueness: true
+      t.string :email, index: { unique: true }
       t.string :password_digest
       t.datetime :last_login
       t.datetime :registration_time
