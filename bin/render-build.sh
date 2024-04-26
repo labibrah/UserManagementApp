82730ad0c666a9cf4a4826fcbe7c5f5ed79c3c22
Labib Rahman <labib.rahman@northsouth.edu>
2024-04-26 16:20:53 +0600

    Added unique index

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-rake db:migrate:reset
+RAILS_ENV=production rake db:migrate:reset
 RAILS_ENV=test rake db:migrate
